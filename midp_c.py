ecd45b2bbef72c975a9f049c6d6f48f8f06b6b23
paulinatorresh <paulimela@icloud.com>
2025-02-12 17:37:07 +0100

    2

--- midp_c.py
+++ midp_c.py
@@ -3,8 +3,8 @@ def get_divisors(n):
     :param n: An integer number
     :return: A list of divisors of n
     """
-    divisors = []  # Create an empty list to store divisors
-    for i in range(1, n + 1):  # Loop from 1 to n
+    divisors = [1, n]  # Create an empty list to store divisors
+    for i in range(2, n//2 + 1):  # Loop from 1 to half of n (no divisors past half)
         if n % i == 0:  # If i divides n exactly (no remainder)
             divisors.append(i)  # Add i to the list of divisors
     return divisors  # Return the list of divisors
